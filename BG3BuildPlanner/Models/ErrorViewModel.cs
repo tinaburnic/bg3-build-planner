b16bba5e9bfe158e281cd18d5a89de6eb2b92757
tinaburnic <tburnic@tvz.hr>
2026-03-19 14:48:40 +0100

    Add project files.

---
+++ BG3BuildPlanner/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace BG3BuildPlanner.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
